713cd297067430b3ee58c12f2cf3ccd35d9e7c9e
sharp-circles <sharpcirclesorg@gmail.com>
2025-11-13 18:13:40 +0100

    Serilog api + handler scaffolding - amoo

---
+++ serilog/owasp.09.serilog.core/UseCases/CreateRoutine/Contracts/CreateRoutineInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace owasp._09.serilog.core.UseCases.CreateRoutine.Contracts;
+
+public record CreateRoutineInput
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public List<ExerciseDetails> Exercises { get; set; }
+}
+
+public record ExerciseDetails
+{
+    public string Name { get; set; }
+    public string Region { get; set; }
+    public int Repetitions { get; set; }
+    public string Description { get; set; }
+}
---
+++ serilog/owasp.09.serilog.core/UseCases/CreateRoutine/CreateRoutineHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using owasp._09.serilog.core.UseCases.CreateRoutine.Contracts;
+
+namespace owasp._09.serilog.core.UseCases.CreateRoutine;
+
+public sealed class CreateRoutineHandler : ICreateRoutineHandler
+{
+    private readonly ILogger<CreateRoutineHandler> _logger;
+
+    public CreateRoutineHandler(ILogger<CreateRoutineHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task Handle(CreateRoutineInput request)
+    {
+        // Hypothetical creation of routine here
+
+        // Log statements
+        _logger.LogInformation("Let's put this log in risk");
+    }
+}
