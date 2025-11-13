ecfeb2c375d780089cdfe62aead09caa3e27e525
sharp-circles <sharpcirclesorg@gmail.com>
2025-11-13 19:54:58 +0100

    De locati con tomati

--- serilog/owasp.09.serilog/Program.cs
+++ serilog/owasp.09.serilog/Program.cs
@@ -1,23 +1,52 @@
-var builder = WebApplication.CreateBuilder(args);
+using owasp._09.serilog.core.UseCases.CreateRoutine;
+using owasp._09.serilog.core.UseCases.CreateRoutine.Contracts;
+using Serilog;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+        .AddEnvironmentVariables()
+        .Build())
+    .Enrich.FromLogContext()
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
+
+    builder.Services.AddControllers();
+
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddScoped<ICreateRoutineHandler, CreateRoutineHandler>();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    Log.Information("Starting web application...");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
